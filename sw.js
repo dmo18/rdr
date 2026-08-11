b3a6cebb59569e3e6a32b6ecbb0bffb083a2b318
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-08-11 17:16:01 -0400

    Remove obsolete weather service worker

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='rdr-radar-v4';
-const CORE=['./','./index.html','./radar.css','./radar.js'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  const u=new URL(event.request.url);
-  if(u.origin!==location.origin)return;
-  event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
-});
